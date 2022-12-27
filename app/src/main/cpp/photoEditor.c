1b24f8f52be0aa5f539ea2eae685462653c2220e
Halieeim <ahmedlpl86@gmail.com>
2022-12-27 11:25:06 +0200

    initial commit

---
+++ app/src/main/cpp/photoEditor.c
@@ -0,0 +1,192 @@
+#include "jni.h"
+#include "math.h"
+#include <stdlib.h>
+
+JNIEXPORT void JNICALL
+Java_com_example_photoeditingapp_MainActivity_blackAndWhite(JNIEnv *env, jclass clazz,
+                                                            jintArray pixels_, jint width,
+                                                            jint height) {
+    jint *pixels = (*env)->GetIntArrayElements(env, pixels_, NULL);
+    char *colors = (char*) pixels;
+    int pixelCount = width * height * 4;
+    for (int i = 0; i < pixelCount; i+=4) {
+        unsigned char average = (colors[i] + colors[i+1] + colors[i+2])/3;
+        colors[i] = average;
+        colors[i+1] = average;
+        colors[i+2] = average;
+    }
+    (*env)->ReleaseIntArrayElements(env,pixels_,pixels,0);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_photoeditingapp_MainActivity_pastelFilter(JNIEnv *env, jclass clazz,
+                                                         jintArray pixels_, jint width,
+                                                         jint height) {
+    // TODO: implement dawnFilter()
+    jint *pixels = (*env)->GetIntArrayElements(env, pixels_, NULL);
+    char *colors = (char*) pixels;
+    int pixelCount = width * height * 4,r,g,b;
+    for (int i = 0; i < pixelCount; i+=4) {
+
+        r = colors[i];
+        g = colors[i+1];
+        b = colors[i+2];
+
+        int tempR = r, tempG = g, tempB = b;
+        int pastelR = (tempR/2) + 127;
+        int pastelG = (tempG/2) + 127;
+        int pastelB = (tempB/2) + 127;
+
+        colors[i] = pastelR;
+        colors[i+1] = pastelG;
+        colors[i+2] = pastelB;
+
+    }
+    (*env)->ReleaseIntArrayElements(env,pixels_,pixels,0);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_photoeditingapp_MainActivity_dilationFilter(JNIEnv *env, jclass clazz,
+                                                             jintArray pixels_, jint width,
+                                                             jint height) {
+    // TODO: implement dilationFilter()
+    jint *pixels = (*env)->GetIntArrayElements(env, pixels_, NULL);
+    char *colors = (char*) pixels;
+    int pixelCount = width*height*4;
+    for (int i = 0; i < pixelCount; i+=4) {
+        colors[i] = (pixels[i] + pixels[i+1] + pixels[i+2])/3;
+    }
+    for (int i = 0; i < pixelCount; i+=4) {
+        if (colors[i] > 100)
+            colors[i] = 255;
+        else
+            colors[i] = 0;
+    }
+    for (int i = 0; i < pixelCount; i+=4) {
+        float value = 0.0f;
+        for (int ky = -1; ky <= 1; ky++) {
+            for (int kx = -1; kx <= 1; kx++) {
+                value += colors[(i+ky)+(i+kx)]/255;
+            }
+        }
+        if (value >= 1)
+            colors[i] = 255;
+        else
+            colors[i] = 0;
+    }
+
+    (*env)->ReleaseIntArrayElements(env,pixels_,pixels,0);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_photoeditingapp_MainActivity_HSBFilter(JNIEnv *env, jclass clazz, jintArray pixels_,
+                                                        jint width, jint height) {
+    // TODO: implement HSBFilter()
+    jint *pixels = (*env)->GetIntArrayElements(env, pixels_, NULL);
+    char *colors = (char*) pixels;
+    int pixelCount = width * height * 4, r,g,b, hue, saturation, brightness;
+    for (int i = 0; i < pixelCount; i+=4) {
+        r = pixels[i];
+        g = pixels[i+1];
+        b = pixels[i+2];
+        double R = r, G = g, B = b;
+        if (G >= R)
+            hue = acos((((R-G)+(R-B))/(sqrt((R-G)*(R-G) + (R-b) + (G-B))))/2);
+        else
+            hue = 360 - acos((((R-G)+(R-B))/(sqrt((R-G)*(R-G) + (R-b) + (G-B))))/2);
+
+        if (R <= G && R <= B)
+            saturation = 1 - (R/(R+G+B))*3;
+        else if (G <= R && G <= B)
+            saturation = 1 - (G/(R+G+B))*3;
+        else
+            saturation = 1 - (B/(R+G+B))*3;
+
+        brightness = (R+G+B)/3;
+        colors[i] = hue;
+        colors[i+1] = saturation;
+        colors[i+2] = brightness;
+    }
+    (*env)->ReleaseIntArrayElements(env,pixels_,pixels,0);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_photoeditingapp_MainActivity_contrastFilter(JNIEnv *env, jclass clazz,
+                                                             jintArray pixels_, jint width,
+                                                             jint height) {
+    // TODO: implement contrastFilter()
+    jint *pixels = (*env)->GetIntArrayElements(env, pixels_, NULL);
+    char *colors = (char*) pixels;
+    int pixelCount = width * height * 4, r,g,b, hue, saturation, brightness;
+    for (int i = 0; i < pixelCount; i+=4) {
+        r = colors[i];
+        g = colors[i+1];
+        b = colors[i+2];
+        if (r >= 100 && r <= 255-20)
+            r += 20;
+        else if (r > 255-20)
+            r = 255;
+        else if (r <= 40 && r >= 20)
+            r-=20;
+        else if (r < 20)
+            r = 0;
+        else if (r > 70 && r < 100)
+            r += 10;
+        else if (r < 60 && r > 40)
+            r -= 10;
+
+        if (g >= 100 && g <= 255-20)
+            g += 20;
+        else if (g > 255-20)
+            g = 255;
+        else if (g <= 40 && g >= 20)
+            g-=20;
+        else if (g < 20)
+            g = 0;
+        else if (g > 70 && g < 100)
+            g += 10;
+        else if (g < 60 && g > 40)
+            g -= 10;
+
+        if (b >= 100 && b <= 255-20)
+            b += 20;
+        else if (b > 255-20)
+            b = 255;
+        else if (b <= 40 && b >= 20)
+            b-=20;
+        else if (b < 20)
+            b = 0;
+        else if (b > 70 && b < 100)
+            b += 10;
+        else if (b < 60 && b > 40)
+            b -= 10;
+
+        colors[i] = r;
+        colors[i+1] = g;
+        colors[i+2] = b;
+    }
+    (*env)->ReleaseIntArrayElements(env,pixels_,pixels,0);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_photoeditingapp_MainActivity_noiseFilter(JNIEnv *env, jclass clazz,
+                                                           jintArray pixels_, jint width,
+                                                           jint height) {
+    // TODO: implement invertFilter()
+    jint *pixels = (*env)->GetIntArrayElements(env, pixels_, NULL);
+    char *colors = (char*) pixels;
+    int pixelCount = width * height * 4, r,g,b, hue, saturation, brightness;
+    for (int i = 0; i < pixelCount; i+=4) {
+        pixels[i] = (pixels[i] + pixels[i+1] + pixels[i+2])/3;
+        int randomNum1 = rand()%100;
+        if (randomNum1 < 5)
+        {
+            int randomNum2 = rand()%2;
+            if (randomNum2 == 0)
+                pixels[i] = 0;
+            else
+                pixels[i] = 255;
+        }
+    }
+    (*env)->ReleaseIntArrayElements(env,pixels_,pixels,0);
+}
--- app/src/main/java/com/example/photoeditingapp/MainActivity.java
+++ app/src/main/java/com/example/photoeditingapp/MainActivity.java
@@ -1,20 +1,38 @@
 package com.example.photoeditingapp;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import kotlin.Suppress;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +40,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (!MainActivity.this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
-        {
-            findViewById(R.id.takePhotoButton).setVisibility(View.GONE);
-        }
+        init();
     }
 
     private static final int REQUEST_PERMISSIONS = 1234;
@@ -48,6 +63,18 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
+    static{
+        System.loadLibrary("photoEditor");
+    }
+
+    // filters
+    private static native void blackAndWhite(int[] pixels, int width, int height);
+    private static native void pastelFilter(int[] pixels, int width, int height);
+    private static native void dilationFilter(int[] pixels, int width, int height);
+    private static native void HSBFilter(int[] pixels, int width, int height);
+    private static native void contrastFilter(int[] pixels, int width, int height);
+    private static native void noiseFilter(int[] pixels, int width, int height);
+
     @Override
     protected void onResume(){  // to check permissions every time the application being opened.
         super.onResume();
@@ -68,20 +95,364 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private static final int REQUEST_PICK_IMAGE = 12345;
+    private ImageView imageView;
+    private int[] originalImg;
     private void init(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        {
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.setVmPolicy(builder.build());
+        }
+        imageView = findViewById(R.id.imageView);
+        if (!MainActivity.this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+        {
+            findViewById(R.id.takePhotoButton).setVisibility(View.GONE);
+        }
         final Button selectImageButton = findViewById(R.id.selectImageButton);
         selectImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
 
+                final Intent pickIntent = new Intent(Intent.ACTION_PICK);
+                pickIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                final Intent chooserIntent = Intent.createChooser(intent,"Select Image");
+                startActivityForResult(chooserIntent,REQUEST_PICK_IMAGE);
             }
         });
         final Button takePhotoButton = findViewById(R.id.takePhotoButton);
         takePhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                final Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+                {
+                    //create a file for the photo just taken
+                    final File photoFile = createImgFile();
+                    imgUri = Uri.fromFile(photoFile);
+                    final SharedPreferences myPrefs = getSharedPreferences(appID, 0);
+                    myPrefs.edit().putString("path",photoFile.getAbsolutePath()).apply();
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Your Camera app is not compatible",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+        final Button backButton = findViewById(R.id.backButtoon);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (findViewById(R.id.editScreen).getVisibility() == View.VISIBLE){
+                    if (findViewById(R.id.filtersBar).getVisibility() == View.VISIBLE){
+                        findViewById(R.id.filtersBar).setVisibility(View.GONE);
+                        findViewById(R.id.optionsBar).setVisibility(View.VISIBLE);
+                    }
+                    else {
+                        findViewById(R.id.editScreen).setVisibility(View.GONE);
+                        findViewById(R.id.welcomeScreen).setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+        final Button filtersButton = findViewById(R.id.filtersButton);
+        filtersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.optionsBar).setVisibility(View.GONE);
+                findViewById(R.id.filtersBar).setVisibility(View.VISIBLE);
+            }
+        });
+
+        final Button B_W_Filter = findViewById(R.id.BlackAndWhite);
+        B_W_Filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    public void run(){
+                        bitmap.getPixels(pixels,0,width,0,0,width, height);
+                        blackAndWhite(pixels,width,height);
+                        bitmap.setPixels(pixels,0,width,0,0,width,height);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }.start();
+                bitmap.setPixels(originalImg,0,width,0,0,width,height);
+            }
+        });
+
+        final Button Pastel_Filter_Button = findViewById(R.id.pastelFilter);
+        Pastel_Filter_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    public void run(){
+                        bitmap.getPixels(pixels,0,width,0,0,width, height);
+                        pastelFilter(pixels,width,height);
+                        bitmap.setPixels(pixels,0,width,0,0,width,height);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }.start();
+                bitmap.setPixels(originalImg,0,width,0,0,width,height);
+            }
+        });
+
+        final Button dilationFilterButton = findViewById(R.id.dilation);
+        dilationFilterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    public void run(){
+                        bitmap.getPixels(pixels,0,width,0,0,width, height);
+                        dilationFilter(pixels,width,height);
+                        bitmap.setPixels(pixels,0,width,0,0,width,height);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }.start();
+                bitmap.setPixels(originalImg,0,width,0,0,width,height);
+            }
+        });
+
+        final Button HSBFilterButton = findViewById(R.id.HSB);
+        HSBFilterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    public void run(){
+                        bitmap.getPixels(pixels,0,width,0,0,width, height);
+                        HSBFilter(pixels,width,height);
+                        bitmap.setPixels(pixels,0,width,0,0,width,height);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }.start();
+                bitmap.setPixels(originalImg,0,width,0,0,width,height);
+            }
+        });
+
+        final Button contrastFilterButton = findViewById(R.id.contrast);
+        contrastFilterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    public void run(){
+                        bitmap.getPixels(pixels,0,width,0,0,width, height);
+                        contrastFilter(pixels,width,height);
+                        bitmap.setPixels(pixels,0,width,0,0,width,height);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }.start();
+                bitmap.setPixels(originalImg,0,width,0,0,width,height);
+            }
+        });
+
+        final Button noiseFilterButton = findViewById(R.id.noise);
+        noiseFilterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    public void run(){
+                        bitmap.getPixels(pixels,0,width,0,0,width, height);
+                        noiseFilter(pixels,width,height);
+                        bitmap.setPixels(pixels,0,width,0,0,width,height);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }.start();
+                bitmap.setPixels(originalImg,0,width,0,0,width,height);
+            }
+        });
+        final Button originalImgButton = findViewById(R.id.originalImage);
+        originalImgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    public void run(){
+                        bitmap.setPixels(originalImg,0,width,0,0,width,height);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }.start();
+            }
+        });
+        final Button saveImageButton = findViewById(R.id.saveImage);
+        saveImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                final DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == DialogInterface.BUTTON_POSITIVE)
+                        {
+                            final File outFile = createImgFile();
+                            try(FileOutputStream out = new FileOutputStream(outFile))
+                            {
+                                bitmap.compress(Bitmap.CompressFormat.JPEG,100,out);
+                                imgUri = Uri.parse("file://" + outFile.getAbsolutePath());
+                                sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, imgUri));
+                                Toast.makeText(MainActivity.this, "The Image was saved", Toast.LENGTH_SHORT).show();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                };
+                builder.setMessage("Save current photo to gallery?").
+                        setPositiveButton("Yes", dialogClickListener).
+                        setNegativeButton("NO", dialogClickListener).show();
+            }
+        });
+    }
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1012;
+    private static final String appID = "photoEditor";
+    private Uri imgUri;
+
+    private File createImgFile(){
+        final String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        final String imgFileName = "/JPEG_" + timeStamp + ".jpg";
+        final File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return new File(storageDir+imgFileName);
+    }
+
+    private boolean editMode = false;
+    private Bitmap bitmap;
+    private int width = 0, height = 0;
+    private static final int MAX_PIXEL_COUNT = 2048;
+
+    private int[] pixels;
+    private int pixelCount = 0;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK)
+        {
+            return;
+        }
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE)
+        {
+            if (imgUri == null)
+            {
+                final SharedPreferences p = getSharedPreferences(appID,0);
+                final String path = p.getString("path","");
+                if (path.length() < 1)
+                {
+                    recreate();
+                    return;
+                }
+                imgUri = Uri.parse("file://" + path);
+            }
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, imgUri));
+        }
+        else if (data == null)
+        {
+            recreate();
+            return;
+        }
+        else if (requestCode == REQUEST_PICK_IMAGE)
+        {
+            imgUri = data.getData();
+        }
+        final ProgressDialog dialog = ProgressDialog.show(MainActivity.this,
+                "Loading", "Please Wait");
+        // we got the image successfully
+        editMode = true;
+        findViewById(R.id.welcomeScreen).setVisibility(View.GONE);
+        findViewById(R.id.editScreen).setVisibility(View.VISIBLE);
+
+        new Thread(){
+            public void run(){
+                bitmap = null;
+                final BitmapFactory.Options bmpOptions = new BitmapFactory.Options();
+                bmpOptions.inBitmap = bitmap;
+                bmpOptions.inJustDecodeBounds = true;
+                try(InputStream input = getContentResolver().openInputStream(imgUri)){
+                    bitmap = BitmapFactory.decodeStream(input, null, bmpOptions);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                bmpOptions.inJustDecodeBounds = false;
+                width = bmpOptions.outWidth;
+                height = bmpOptions.outHeight;
+                int resizeScale = 1;
+                if (width > MAX_PIXEL_COUNT)
+                {
+                    resizeScale = width / MAX_PIXEL_COUNT;
+                }else if (height > MAX_PIXEL_COUNT)
+                {
+                    resizeScale = height / MAX_PIXEL_COUNT;
+                }
+
+                if ((width/resizeScale > MAX_PIXEL_COUNT) || (height/MAX_PIXEL_COUNT > MAX_PIXEL_COUNT))
+                {
+                    resizeScale++;
+                }
+                bmpOptions.inSampleSize = resizeScale;
+                InputStream input = null;
+                try{
+                    input = getContentResolver().openInputStream(imgUri);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    recreate();
+                    return;
+                }
+                bitmap = BitmapFactory.decodeStream(input, null, bmpOptions);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageView.setImageBitmap(bitmap);
+                        dialog.cancel();
+                    }
+                });
+                width = bitmap.getWidth();
+                height = bitmap.getHeight();
+                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888,true);
+                pixelCount = width * height;
+                pixels = new int[pixelCount];
+                originalImg = new int[pixelCount];
+                bitmap.getPixels(pixels,0,width,0,0,width, height);     // image that will be modified
+                bitmap.getPixels(originalImg,0,width,0,0,width,height); // to keep original image
+            }
+        }.start();
     }
 }
